9a9763207c3842ea8bc595652988dfacbb6453d6
tcn741 <tianchenlko@126.com>
2022-04-25 22:19:55 +0800

    基于JDK和Cglib动态代理，实现AOP核心功能

--- tjspring/tj-spring-step-02/src/test/java/com/tj/exercise/springframework/test/TwelveChapterApiTest.java
+++ tjspring/tj-spring-step-02/src/test/java/com/tj/exercise/springframework/test/TwelveChapterApiTest.java
@@ -2,16 +2,21 @@ package com.tj.exercise.springframework.test;
 
 import com.sun.org.apache.xml.internal.utils.res.XResources_ja_JP_HI;
 import com.tj.exercise.springframework.aop.AdvisedSupport;
+import com.tj.exercise.springframework.aop.MethodMatcher;
 import com.tj.exercise.springframework.aop.TargetSource;
 import com.tj.exercise.springframework.aop.aspectj.AspectJExpressionPointcut;
 import com.tj.exercise.springframework.aop.framework.Cglib2AopProxy;
 import com.tj.exercise.springframework.aop.framework.JdkDynamicAopProxy;
+import com.tj.exercise.springframework.aop.framework.ReflectiveMethodInvocation;
 import com.tj.exercise.springframework.test.bean.IUserService;
 import com.tj.exercise.springframework.test.bean.TwelveChapterUserService;
 import com.tj.exercise.springframework.test.bean.UserServiceInterceptor;
+import org.aopalliance.intercept.MethodInterceptor;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * @Author: tj
@@ -47,5 +52,45 @@ public class TwelveChapterApiTest {
 
         System.out.println("测试结果: " + proxy_cglib.register("test"));
 
+    }
+
+    @Test
+    public void test_proxy_class(){
+        IUserService userService = (IUserService) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] {IUserService.class}, (proxy, method, args) -> "你被代理了！" );
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果: " + result);
+    }
+
+    @Test
+    public void test_proxy_method() {
+        Object targetObj = new TwelveChapterUserService();
+
+        MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* com.tj.exercise.springframework.test.bean.IUserService.*(..))");
+
+
+        IUserService proxy = (IUserService) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), targetObj.getClass().getInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if(methodMatcher.matches(method,targetObj.getClass())) {
+                    MethodInterceptor methodInterceptor = invocation ->{
+                        long start = System.currentTimeMillis();
+                        try{
+                            return invocation.proceed();
+                        } finally {
+                            System.out.println("监控 - Begin By AOP");
+                            System.out.println("方法名称: " + invocation.getMethod().getName());
+                            System.out.println("方法耗时: " +(System.currentTimeMillis() - start) +"ms");
+                            System.out.println("监控 - End\r\n");
+                        }
+                    };
+
+                    return methodInterceptor.invoke(new ReflectiveMethodInvocation(targetObj,method,args));
+                }
+                return method.invoke(targetObj,args);
+            }
+        });
+
+        String result = proxy.queryUserInfo();
+        System.out.println("测试结果: " +result);
     }
 }
